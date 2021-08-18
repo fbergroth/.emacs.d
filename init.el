719f0b679579421c3d2ea5136c34fdfbc4241940
Fredrik Bergroth <fbergroth@gmail.com>
2021-08-18 13:35:28 +0200

    Setup vertico-directory

--- init.el
+++ init.el
@@ -173,6 +173,16 @@
   :init
   (vertico-mode))
 
+(use-package vertico-directory
+  ;; More convenient directory navigation commands
+  :load-path "lib/vertico/extensions"
+  :bind (:map vertico-map
+              ("RET" . vertico-directory-enter)
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+  ;; Tidy shadowed file names
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
 (use-package orderless
   :demand t
   :config
